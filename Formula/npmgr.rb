8aff9cea443bfc664cefa6858f73e2d3860f5e1d
ramanavelineni <rcvelineni@gmail.com>
2026-02-15 09:28:43 -0800

    Update npmgr to v1.0.0 with MIT License

--- Formula/npmgr.rb
+++ Formula/npmgr.rb
@@ -6,20 +6,20 @@ class Npmgr < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ramanavelineni/npmgr/releases/download/v1.0.0/npmgr_1.0.0_darwin_arm64.tar.gz"
-      sha256 "4f7abec90df8ee2964e1aa06aee0fac83119a329af2132317f2033e14eb8e89d"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
       url "https://github.com/ramanavelineni/npmgr/releases/download/v1.0.0/npmgr_1.0.0_darwin_amd64.tar.gz"
-      sha256 "42bc5e0ff84c334bbdbfa6d89838453b088b5c8de4e8f5df7cace810b4afaf70"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/ramanavelineni/npmgr/releases/download/v1.0.0/npmgr_1.0.0_linux_arm64.tar.gz"
-      sha256 "404fc9178cecc0fda1518bfde406d1571c174dc75b2f62436edec71d4bc3f41d"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
       url "https://github.com/ramanavelineni/npmgr/releases/download/v1.0.0/npmgr_1.0.0_linux_amd64.tar.gz"
-      sha256 "3414c5d83dffc24699a48036f344abc419d2b1163480e2d65fa56abd639bd64b"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
